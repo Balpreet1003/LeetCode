ebc430b0fb6878fe32f37d7b2de9f9ba6e496397
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2024-09-11 17:21:00 +0530

    Time: 3 ms (70.88%), Space: 12.5 MB (7.2%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
         int i=0,j=nums.size()-1,ans=0;
         while(i<=j){
             int mid=(i+j)/2;
