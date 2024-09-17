f97a4554baf444a011ac7f78e8ef0c99dc93d85f
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2024-09-17 17:26:26 +0530

    Time: 102 ms (78.23%), Space: 13.9 MB (86.14%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    bool ifPartition(vector<int>&nums,int target, int index,vector<vector<int> >&dp){
+        int n=nums.size();
+        if(target==0 )
+            return 1;
+        if(target<0 || index>=n )
+            return 0;
+
+        if(dp[index][target] !=-1)
+            return dp[index][target];
+
+        //include
+        bool include=0;
+        if((target-nums[index])>=0)
+            include = ifPartition(nums,target-nums[index],index+1,dp);
+        bool exclude = ifPartition(nums,target,index+1,dp);
+        dp[index][target] = (include || exclude);
+        return dp[index][target];
+    }
+
+    bool ifPartition(vector<int>&nums,int & target){
+        int n=nums.size();
+        // vector<vector<int> >dp(n+1,vector<int>(target+1,0));
+        vector<int>curr(target+1,0);
+        vector<int>next(target+1,0);
+
+        curr[0]=1;
+        next[0]=1;
+
+        for(int index=n-1;index>=0;index--){
+            for(int tar=1;tar<=target;tar++){
+                bool include=0;
+                if((tar-nums[index])>=0)
+                    include = next[tar-nums[index]];
+                bool exclude = next[tar];
+                curr[tar] = (include || exclude);   
+            }
+            //shift
+            next=curr;
+        } 
+        return curr[target];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int i =0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum & 1)
+            return false;
+        int target=sum/2;
+        int index=0;
+        // vector<vector<int> >dp(nums.size()+2,vector<int>(target+1,-1));
+        // return ifPartition(nums,target,index,dp);
+
+        return ifPartition(nums,target);
+    }
+};
