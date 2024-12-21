46a3a29aa5725a5ba57ec1ac69e9182211a8b393
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2024-12-22 02:49:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,15 +1,34 @@
 class Solution {
+    int maxi(vector<vector<int>>&mat,int j){
+        int ans=0;
+        for(int i=1;i<mat.size();i++){
+            if(mat[i][j]>mat[ans][j])ans=i;
+        }
+        return ans;
+    }
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                bool cond=true;
-                if(i>0 && mat[i-1][j]>=mat[i][j])cond=false;
-                if(i<(n-1) && mat[i+1][j]>=mat[i][j])cond=false;
-                if(j>0 && mat[i][j-1]>=mat[i][j])cond=false;
-                if(j<(m-1) && mat[i][j+1]>=mat[i][j])cond=false;
-                if(cond)return {i,j};
+        if(n==1 && m==1)return {0,0};
+        if(m==1){
+            return {maxi(mat,0),0};
+        }
+        int i=0,j=m-1;
+        while(i<=j){
+            int mid=(j+i)/2;
+            int x=maxi(mat,mid);
+            if(mid==0){
+                if(mat[x][mid]>mat[x][mid+1])return {x,mid};
+                else i=mid+1;
+            }
+            else if(mid==m-1){
+                if(mat[x][mid]>mat[x][mid-1])return {x,mid};
+                else j=mid-1;
+            }
+            else {
+                if(mat[x][mid]>mat[x][mid-1] && mat[x][mid]>mat[x][mid+1])return {x,mid};
+                else if(mat[x][mid]<mat[x][mid-1])j=mid-1;
+                else j=mid+1;
             }
         }
         return {-1,-1};
