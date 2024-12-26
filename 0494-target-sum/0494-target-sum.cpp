9e12abe32a750e36aedd0b8e3805c56fca8f54ab
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2024-12-26 21:02:27 +0530

    Time: 8 ms (67.51%), Space: 25.2 MB (28.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,16 @@
 class Solution {
-private:
-    int n=1000;
-    int getNumberOfWays(vector<int>&nums,int i ,int& target,int sum,vector<vector<int>>&dp){
-        //bc
-        if(sum==target && i==nums.size())
-            return 1;
-        if(i>=nums.size())
-            return 0;
-
-        if(dp[i][sum+n]!=-1)
-            return dp[i][n+sum];
-        //1 case
-        //for +
-        int plus=getNumberOfWays(nums,i+1,target,sum+nums[i],dp);
-        //for -
-        int minus=getNumberOfWays(nums,i+1,target,sum-nums[i],dp);
-        return dp[i][n+sum] = plus+minus;
+    int x=0;
+    int solve(vector<int>&a,int i,int& t,int sum,vector<vector<int>>&dp){
+        if(sum==t && i==a.size())return 1;
+        if(i>=a.size()) return 0;
+        if(dp[i][sum+x]!=-1)return dp[i][sum+x];
+        return dp[i][x+sum]=solve(a,i+1,t,sum+a[i],dp)+solve(a,i+1,t,sum-a[i],dp);
     }
-
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(2*n+1,-1));
-        return getNumberOfWays(nums,0,target,0,dp);
+    int findTargetSumWays(vector<int>& a, int t) {
+        int n=a.size();
+        x=accumulate(a.begin(),a.end(),0);
+        vector<vector<int>>dp(n+1,vector<int>(2*x+1,-1));
+        return solve(a,0,t,0,dp);
     }
 };
