d7fe6c055d38febf8a17d9ae1a1b3e736da0125a
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-02-06 13:17:16 +0530

    Time: 30 ms (60.52%), Space: 44.2 MB (33.28%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,37 +1,23 @@
 class Solution {
-    vector<vector<int>> dp;
-    int solve(string& s, string& t, int i, int j) {
-        if (j >= t.length())
-            return 1;
-        if (i >= s.length())
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int cnt = 0;
-        if (s[i] == t[j])
-            cnt += solve(s, t, i + 1, j + 1);
-        return dp[i][j] = cnt + solve(s, t, i + 1, j);
-    }
+    vector<vector<double>> dp;
 
 public:
     int numDistinct(string s, string t) {
         int n = s.length(), m = t.length();
-        dp = vector<vector<int>>(n + 1, vector<int>(m + 1, -1));
+        dp = vector<vector<double>>(n + 1, vector<double>(m + 1, 0));
 
-        // for (int i = 0; i <= n; i++)
-        //     dp[i][m] = 1;
+        for (int i = 0; i <= n; i++)
+            dp[i][m] = 1;
 
-        // for (int i = n - 1; i >= 0; i--) {
-        //     for (int j = m - 1; j >= 0; j--) {
-        //         int cnt = 0;
-        //         if (s[i] == t[j])
-        //             cnt += dp[i + 1][j + 1];
-        //         dp[i][j] = cnt + dp[i + 1][j];
-        //     }
-        // }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int cnt = 0;
+                if (s[i] == t[j])
+                    dp[i][j]= dp[i + 1][j + 1]+dp[i + 1][j];
+                else dp[i][j] = dp[i + 1][j];
+            }
+        }
 
-        return solve(s,t,0,0);
+        return dp[0][0];
     }
 };
