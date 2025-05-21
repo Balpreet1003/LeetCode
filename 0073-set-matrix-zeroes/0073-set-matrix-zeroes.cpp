63073166832e4129fcfb0f4c330873d95a72db86
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-05-21 14:54:37 +0530

    Time: 0 ms (100%), Space: 18.7 MB (52.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-
-        int n=nums.size(),m=nums[0].size();
-        unordered_map<int,int>row,col;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(nums[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n1=matrix.size(),n2=matrix[0].size();
+        unordered_map<int,int>mi,mj;
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(matrix[i][j]==0){
+                    mi[i]=1;
+                    mj[j]=1;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row.find(i)!=row.end())nums[i][j]=0;
-                if(col.find(j)!=col.end())nums[i][j]=0;
+
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(mi.find(i)!=mi.end() || mj.find(j)!=mj.end())matrix[i][j]=0;
             }
         }
     }
