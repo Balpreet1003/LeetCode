6ec9d1c1872daadd1535290888ee8aadcf6d2bd0
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-05-29 21:18:41 +0530

    Time: 0 ms (100%), Space: 10.5 MB (52.42%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,17 +1,26 @@
 class Solution {
+    vector<string>ans;
+    string s="";
+    void solve(int& n,int& k){
+        if(s.length()>=n){
+            ans.push_back(s);
+            return;
+        }
+        if(ans.size()>=k)return ;
+
+        for(int i=0;i<3;i++){
+            char ch=char(i+'a');
+            if(s!="" && s.back()==ch)continue;
+
+            s.push_back(ch);
+            solve(n,k);
+            s.pop_back();
+        }
+    }
 public:
     string getHappyString(int n, int k) {
-        int sz=3*(1<<(n-1));
-        if (k>sz) return "";
-        auto [q, r]=div(k-1, 1<<(n-1));
-        string s(n, ' ');
-        s[0]='a'+q;
-        bitset<9> bin(r);
-        array<char, 2> xx[3]={{'b', 'c'}, {'a', 'c'}, {'a', 'b'}};
-        for(int i=n-2; i>=0; i--){
-            char idx=s[n-2-i]-'a';
-            s[n-1-i]=(bin[i])?xx[idx][1]:xx[idx][0];
-        }
-        return s;
+        solve(n,k);
+        if(ans.size()<k)return "";
+        return ans[k-1];
     }
 };
