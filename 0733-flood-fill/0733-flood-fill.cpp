b6ddb8c03e3a40df792528fa35179cc22e7310d4
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-06-02 15:21:57 +0530

    Time: 0 ms (100%), Space: 18.2 MB (48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,22 @@
 class Solution {
+    vector<int>dx={-1,0,1,0};
+    vector<int>dy={0,1,0,-1};
+    void dfs(vector<vector<int>>&nums,vector<vector<bool>>&visited,int i,int j,int&color,int initial_color) {
+        if(i<0 || i>=nums.size() || j<0 || j>=nums[0].size() || visited[i][j] || nums[i][j]!=initial_color)return;
+
+        nums[i][j]=color;
+        visited[i][j]=true;
+
+        for(int k=0;k<4;k++){
+            dfs(nums,visited,i+dx[k],j+dy[k],color,initial_color);
+        }
+    }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
         int n=img.size(),m=img[0].size();
         vector<vector<int>>ans=img;
         vector<vector<bool>>visited(n,vector<bool>(m,false));
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        ans[sr][sc]=color;
-
-        vector<int>dx={-1,0,1,0};
-        vector<int>dy={0,1,0,-1};
-
-        while(!q.empty()){
-            auto fNode=q.front();
-            q.pop();
-            
-            for(int i=0;i<4;i++){
-                int x1=fNode.first+dx[i];
-                int y1=fNode.second+dy[i];
-
-                if(x1>=0 && x1<n && y1>=0 && y1<m && ans[x1][y1]==img[sr][sc] && !visited[x1][y1]){
-                    ans[x1][y1]=color;
-                    q.push({x1,y1});
-                    visited[x1][y1]=true;
-                }
-            }
-        }
+        dfs(ans,visited,sr,sc,color,img[sr][sc]);
         return ans;
     }
 };
