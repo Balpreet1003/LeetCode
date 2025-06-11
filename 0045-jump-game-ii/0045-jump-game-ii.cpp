145e00c8fcc7d255f42cec6ed4a0297f87436c5e
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-06-11 15:39:52 +0530

    Time: 675 ms (5%), Space: 28.5 MB (7.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
-    vector<int>dp;
+    unordered_map<long long ,int>dp;
     int solve(vector<int>&nums,int i){
         if(i==nums.size()-1)return 0;
         if(i>=nums.size() || nums[i]==0)return INT_MAX;
-        if(dp[i]!=-1)return dp[i];
+        if(dp.find(i)!=dp.end())return dp[i];
 
         int ans=INT_MAX;
         for(int j=1;j<=nums[i];j++){
@@ -12,8 +12,7 @@ class Solution {
         return dp[i]=(ans==INT_MAX) ? ans : ans+1;
     }
 public:
-    int jump(vector<int>& nums) {
-        dp=vector<int>(nums.size()+*max_element(nums.begin(),nums.end())+1,-1);
+    int jump(vector<int>& nums) {        
         return solve(nums,0);
     }
 };
