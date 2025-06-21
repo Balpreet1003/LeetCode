d20d77552d60d2a073e80ead80623422c7b6b023
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-06-21 23:32:22 +0530

    Time: 17 ms (50.9%), Space: 193.7 MB (71.96%) - LeetHub

--- 3587-minimum-adjacent-swaps-to-alternate-parity/3587-minimum-adjacent-swaps-to-alternate-parity.cpp
+++ 3587-minimum-adjacent-swaps-to-alternate-parity/3587-minimum-adjacent-swaps-to-alternate-parity.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    int count(vector<int> nums)
-    {
-        int ans=0;
-        for(int i=0;i<nums.size();i++)
-            ans += abs(nums[i]-i*2); 
-        return ans;
-    }
-    int minSwaps(vector<int>& nums) 
-    {
-        vector<int> e,o;
-        for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]%2)
-                    o.push_back(i);
-                else
-                    e.push_back(i);
-            }
-        if(abs((int)o.size()-(int)e.size())>1)
-            return -1;
-        int ans = INT_MAX;
-        if(o.size()>=e.size())
-            ans = min(ans,count(o));
-        if(e.size() >= o.size())
-            ans = min(ans,count(e));
-        return ans;
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        int even = 0, odd = 0;
+
+        for(int num : nums) {
+            if(num % 2 == 0) even++;
+            else odd++;
+        }
+
+        if(abs(even - odd) > 1) return -1;
+
+        vector<int> even_pos, odd_pos;
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] % 2 == 0) even_pos.push_back(i);
+            else odd_pos.push_back(i);
+        }
+
+        int res = INT_MAX;
+
+        if(even >= odd) {
+            int swaps = 0;
+            for(int i = 0; i < even_pos.size(); ++i)
+                swaps += abs(even_pos[i] - 2 * i);
+            res = min(res, swaps);
+        }
+
+        if(odd >= even) {
+            int swaps = 0;
+            for(int i = 0; i < odd_pos.size(); ++i)
+                swaps += abs(odd_pos[i] - 2 * i);
+            res = min(res, swaps);
+        }
+
+        return res;
     }
 };
