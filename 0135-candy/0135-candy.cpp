b4c4060e224abf3120f563635b72672acf29be8b
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-06-28 13:57:21 +0530

    Time: 411 ms (5.03%), Space: 23 MB (45.95%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        if(ratings.size()==1)return 1;
         int n=ratings.size();
-        vector<int>ans(n,1);
-
+        vector<int>sum(n,1);
         for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1])ans[i]=ans[i-1]+1;
+            if(ratings[i]>ratings[i-1]){
+                sum[i]=sum[i-1]+1;
+            }
+            // else if(ratings[i]==ratings[i-1]){
+            //     sum[i]=sum[i-1];
+            // }
         }
-        for(int i=n-1;i>0;i--){
-            if(ratings[i-1]>ratings[i])ans[i-1]=max(ans[i-1],ans[i]+1);
+        for(int i:sum){
+            cout<<i<<" ";
+        }cout<<endl;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                if(sum[i]<=sum[i+1]){
+                    sum[i]=sum[i+1]+1;
+                }
+                // else if(ratings[i]==ratings[i+1]){
+                //     sum[i]=sum[i+1];
+                // }
+            }
         }
-        int sum=accumulate(ans.begin(),ans.end(),0);
-        return sum;
+        for(int i:sum){
+            cout<<i<<" ";
+        }cout<<endl;
+        return accumulate(sum.begin(),sum.end(),0);
     }
 };
