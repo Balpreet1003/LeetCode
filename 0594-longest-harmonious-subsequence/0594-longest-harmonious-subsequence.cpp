b9a02fbc7ead71bcc9d6e2196d1f965b6be02928
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-06-30 11:12:13 +0530

    Time: 37 ms (30.97%), Space: 43.9 MB (29.65%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -7,9 +7,6 @@ public:
             m[i]++;
         }
         for(auto &[x,y]:m){
-            if(x!=(-1e9) && m.find(x-1)!=m.end()){
-                ans=max(ans,y+m[x-1]);
-            }
             if(x!=(1e9) && m.find(x+1)!=m.end()){
                 ans=max(ans,y+m[x+1]);
             }
