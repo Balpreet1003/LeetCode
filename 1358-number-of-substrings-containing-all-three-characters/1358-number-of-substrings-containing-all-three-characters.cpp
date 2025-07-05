3c4f9951e137c10bc74debe8e1d240499634bead
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-07-05 16:06:41 +0530

    Time: 0 ms (100%), Space: 11 MB (79.72%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char,int>m;
-        int i=0,j=0,ans=0,n=s.length();
-        while(j<n){
-            if(m.size()>=3){
-                ans+=(n-j+1);
-                m[s[i]]--;
-                if(m[s[i]]<=0)m.erase(s[i]);
+        int n=s.length(),i=0,ans=0;
+        vector<int>m(3,0);
+        for(int j=0;j<n;j++){
+            m[s[j]-'a']++;
+            while(m[0]>0 && m[1]>0 &&m[2]>0){
+                ans+=(n-j);
+                m[s[i]-'a']--;
                 i++;
             }
-            else{
-                m[s[j]]++;
-                j++;
-            }
-        }
-        while(m.size()>=3){
-            ans+=(n-j+1);
-            m[s[i]]--;
-            if(m[s[i]]<=0)m.erase(s[i]);
-            i++;
         }
         return ans;
     }
