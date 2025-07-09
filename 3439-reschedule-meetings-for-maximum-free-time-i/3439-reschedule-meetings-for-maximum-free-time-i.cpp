061170ad3d6f6f74a1703cd89e6e722145dbbc51
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-07-09 22:50:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        vector<int>nums;
+        int n=startTime.size();
+        if(startTime[0]>0)nums.push_back(startTime[0]-0);
+        for(int i=1;i<n;i++){
+            nums.push_back(startTime[i]-endTime[i-1]);
+        }
+        if(endTime[n-1]<eventTime)nums.push_back(eventTime-endTime[0]);
+
+        int ans=0;
+        int i=0,j=0,sum=0;
+        while(j<nums.size()){
+            if((j-i+1)<=(k+1)){
+                sum+=nums[j];
+                j++;
+            }
+            else{
+               sum-=nums[i];
+               i++; 
+            }
+            ans=max(ans,sum);
+        }
+        return ans;
+    }
+};
