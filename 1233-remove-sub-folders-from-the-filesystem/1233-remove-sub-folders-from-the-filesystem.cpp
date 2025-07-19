d3057650269390c3707fef8fbf58fdd917fc3048
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-07-19 12:19:15 +0530

    Time: 83 ms (47.96%), Space: 71.5 MB (39.57%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,14 +1,30 @@
 class Solution {
+    static bool cmd(string&s1,string&s2){
+        return s1.length()<s2.length();
+    }
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        vector<string> ans;
-        ans.push_back(folder[0]);
-        for(int i = 1; i < folder.size(); i++) {
-            string lastFolder = ans.back();
-            lastFolder.push_back('/');
-            if(folder[i].compare(0, lastFolder.size(), lastFolder) != 0) {
-                ans.push_back(folder[i]);
+        int n=folder.size();
+        vector<bool>trace(n,true);
+        sort(folder.begin(),folder.end(),cmd);
+        vector<string>ans;
+        unordered_map<string,int>m;
+        for(int j=0;j<n;j++){
+            string s=folder[j];
+            string s1="";
+            for(int i=0;i<s.length();){
+                s1+=s[i++];
+                while(i<s.length() && s[i]!='/'){
+                    s1+=s[i++];
+                }
+                if(m.find(s1)!=m.end()){
+                    trace[j]=false;
+                    break;
+                }
+            }
+            m[s]=1;
+            if(trace[j]){
+                ans.push_back(s);
             }
         }
         return ans;
