2e0badcc3407429f465d034f69a59a5e995115a0
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-07-27 18:55:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (99.65%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int minBitFlips(int n1, int n2) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        int count=0;
-        while(n1 || n2){
-            bool p1=n1&1,p2=n2&1;
-            if(p1!=p2)count++;
-            n1=n1>>1;
-            n2=n2>>1;
+    int minBitFlips(int start, int goal) {
+        int x=start^goal, cnt=0;
+        while(x){
+            if(x&1)cnt++;
+            x>>=1;
         }
-        return count;
+        return cnt;
     }
 };
