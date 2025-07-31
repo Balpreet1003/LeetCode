3edf8d33eae6eb8d3faacb52f72ea042769ed419
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-01 00:34:16 +0530

    Time: 0 ms (100%), Space: 18 MB (20.2%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    bool search(vector<int>& a, int t) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        int i=0,j=a.size()-1;
+    bool search(vector<int>& nums, int target) {
+        if(nums.size()==1){
+            return nums[0]==target ? true : false;
+        }
+        int i=0,j=nums.size()-1;
         while(i<=j){
             int mid=(i+j)/2;
-            if(a[mid]==t)return true;
-            else if(a[i]==a[mid] && a[mid]==a[j]){
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[i]==nums[mid] && nums[mid]==nums[j]){
                 i++;
                 j--;
+                continue;
             }
-            else if(a[i]<=a[mid]){
-                if(a[i]<=t && a[mid]>=t){
+            else if(nums[mid]<nums[i]){
+                if(nums[mid]<=target && nums[j]>=target){
+                    i=mid+1;
+                }
+                else{
                     j=mid-1;
                 }
-                else i=mid+1;
             }
             else{
-                if(a[mid]<=t && a[j]>=t)i=mid+1;
-                else j=mid-1;
+                if(nums[mid]>=target && nums[i]<=target){
+                    j=mid-1;
+                }
+                else{
+                    i=mid+1;
+                }
             }
         }
         return false;
