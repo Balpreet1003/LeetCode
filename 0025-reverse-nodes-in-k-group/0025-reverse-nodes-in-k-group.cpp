c217cea0b4dd64b8aaf9d4038762ffc911cb6b60
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-04 20:09:33 +0530

    Time: 0 ms (100%), Space: 16.6 MB (39.13%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,37 +8,42 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
-    // Function to reverse elements between index i and j in vector a
-    void rev(vector<ListNode*>& a, int i, int j) {
-        while (i < j) swap(a[i++], a[j--]);
+    int get_len(ListNode* head){
+        ListNode* node=head;
+        int ans=0;
+        while(node){
+            node=node->next;
+            ans++;
+        }
+        return ans;
     }
-    
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || !head->next || k == 1) return head; // Base cases
-        
-        vector<ListNode*> a;
-        
-        // Store all nodes in the vector
-        while (head) {
-            a.push_back(head);
-            head = head->next;
+        if (k == 1) {
+            return head;
         }
-        
-        // Reverse every group of size k
-        for (int i = 0; i + k <= a.size(); i += k) {
-            rev(a, i, i + k - 1);
+        int len=get_len(head);
+        int t=len/k;
+        ListNode* node=head, *tail=NULL;
+        while(t--){
+            ListNode* head1=node, *prev=NULL, *tail1=node;
+            int temp=k;
+            while(node && temp--){
+                head1=node;
+                node=node->next;
+                head1->next=prev;
+                prev=head1;
+            }
+            if(tail){
+                tail->next=head1;
+            }
+            tail=tail1;
+            if(tail==head){
+                head=head1;
+            }
         }
-        
-        // Rebuild the linked list from the modified vector
-        head = a[0];
-        for (int i = 1; i < a.size(); i++) {
-            a[i - 1]->next = a[i];
-        }
-        a[a.size() - 1]->next = nullptr; // Ensure the last node points to null
-        
+        tail->next=node;
         return head;
     }
 };
