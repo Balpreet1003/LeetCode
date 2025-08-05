45b4284c0edf6bd59c09ffdd8ebf77760881f167
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-05 19:33:31 +0530

    Time: 0 ms (100%), Space: 9.2 MB (26.23%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,21 @@
 class Solution {
     vector<vector<int>>ans;
-    void solve(int&k,int&n,int i,int sum,vector<int>&a){
-        if(a.size()==k && sum==n){
+    vector<int>a;
+    void solve(int&k,int n,int i){
+        if(a.size()==k && n==0){
             ans.push_back(a);
             return;
         }
         if(i>9 || a.size()>=k)return;
 
         a.push_back(i);
-        solve(k,n,i+1,sum+i,a);
+        solve(k,n-i,i+1);
         a.pop_back();
-        solve(k,n,i+1,sum,a);
+        solve(k,n,i+1);
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        vector<int>a;
-        solve(k,n,1,0,a);
+        solve(k,n,1);
         return ans;
     }
 };
