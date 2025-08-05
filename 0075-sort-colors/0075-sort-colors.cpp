9226a32a4cf9eeeabd3c506e8e63cbcfa17a976c
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-05 23:46:56 +0530

    Time: 4 ms (1.47%), Space: 9.9 MB (99.97%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+       int redCount=0,whiteCount=0,blueCount=0;
+       for(int i=0;i<nums.size();i++){
+           if(nums[i]==0)redCount++;
+           if(nums[i]==1)whiteCount++;
+           if(nums[i]==2)blueCount++;
+       }
+       int k=0;
+       while(redCount--){
+           nums[k]=0;
+           k++;
+       }
+       while(whiteCount--){
+           nums[k]=1;
+           k++;
+       }
+       while(blueCount--){
+           nums[k]=2;
+           k++;
+       }
     }
 };
