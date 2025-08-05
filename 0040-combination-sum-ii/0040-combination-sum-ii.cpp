64d169a3e352935a9ff8a93921304312c72a4964
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-05 16:52:21 +0530

    Time: 3 ms (40.45%), Space: 14.2 MB (30.04%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,29 +1,26 @@
 class Solution {
+    vector<vector<int>>ans;
+    vector<int>temp;
 private:
-    void getCombination(vector<int>& nums,int target,vector<vector<int>>&ans,vector<int>&temp,int i){
-        //bc
+    void getCombination(vector<int>& nums,int target,int i){
         if(target==0){
             ans.push_back(temp);
             return;
         }
         if(target<0)
             return;
-
-        //1 case
         for(int k=i;k<nums.size();k++){
             if (k > i && nums[k] == nums[k - 1]) continue;
             if (nums[k] > target) break;
             temp.push_back(nums[k]);
-            getCombination(nums,target-nums[k],ans,temp,k+1);
+            getCombination(nums,target-nums[k],k+1);
             temp.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        vector<int>temp;
-        getCombination(nums,target,ans,temp,0);
+        getCombination(nums,target,0);
         return ans;
     }
 };
