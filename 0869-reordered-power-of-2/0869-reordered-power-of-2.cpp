2c2b910f039366388de5f513842e92cdfbc4bafc
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-10 11:36:28 +0530

    Time: 0 ms (100%), Space: 9.1 MB (41.89%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -1,38 +1,18 @@
 class Solution {
-    unordered_map<int, vector<unordered_map<char,int>>> m;
-    
-    void get_map(string x) {
-        unordered_map<char,int> m1;
-        for(char c : x) {
-            m1[c]++;
-        }
-        m[x.length()].push_back(m1);
+    string sortDigits(int num) {
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        return s;
     }
-    
-    bool is_match(unordered_map<char,int>& m1, unordered_map<char,int>& m2) {
-        if(m1.size() != m2.size()) return false;
-        for(auto &[x,y] : m1) {
-            if(m2.find(x) == m2.end() || m2[x] != y) return false;
-        }
-        return true;
-    }
-    
 public:
     bool reorderedPowerOf2(int n) {
-        long long x = 1;
-        while(x <= 1e9) {
-            get_map(to_string(x));
-            x <<= 1;
-        }
+        string s = sortDigits(n);
 
-        string s = to_string(n);
-        unordered_map<char,int> m1;
-        for(char c : s) m1[c]++;
-        
-        if(m.find(s.length()) == m.end()) return false;
-        
-        for(auto &mp : m[s.length()]) {
-            if(is_match(m1, mp)) return true;
+        for(int i = 0; i<31; i++){
+            int power = 1 << i;
+            if(s == sortDigits(power)){
+                return true;
+            }
         }
         return false;
     }
