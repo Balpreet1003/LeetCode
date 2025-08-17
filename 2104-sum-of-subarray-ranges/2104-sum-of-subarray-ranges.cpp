e675826a3b92f2c3ed0935c376509d931bbd9e38
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-18 00:11:06 +0530

    Time: 4 ms (60.81%), Space: 15.7 MB (54.94%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,16 +1,63 @@
 class Solution {
-public:
-    long long subArrayRanges(vector<int>& nums) {
+    long long get_max_sum(vector<int>& nums){
         int n=nums.size();
+        stack<int>st;
+        vector<int>prev(n,-1),next(n,-1);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i]){
+                st.pop();
+            }
+            prev[i]=st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty())st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]<=nums[i]){
+                st.pop();
+            }
+            next[i]=st.empty() ? n : st.top();
+            st.push(i); 
+        }
         long long ans=0;
         for(int i=0;i<n;i++){
-            long long mini=INT_MAX,maxi=INT_MIN;
-            for(int j=i;j<n;j++){
-                mini=min(mini,(long long)nums[j]);
-                maxi=max(maxi,(long long)nums[j]);
-                ans+=(maxi-mini);
-            }
+            long long x=i-prev[i];
+            long long y=next[i]-i;
+            ans+=x*y*nums[i];
         }
         return ans;
+    }
+    long long get_min_sum(vector<int>& nums){
+        int n=nums.size();
+        stack<int>st;
+        vector<int>prev(n,-1),next(n,-1);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()]>nums[i]){
+                st.pop();
+            }
+            prev[i]=st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while(!st.empty())st.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>=nums[i]){
+                st.pop();
+            }
+            next[i]=st.empty() ? n : st.top();
+            st.push(i); 
+        }
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            long long x=i-prev[i];
+            long long y=next[i]-i;
+            ans+=x*y*nums[i];
+        }
+        return ans;
+    }
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long max_sum=get_max_sum(nums);
+        long long min_sum=get_min_sum(nums);
+
+        return max_sum-min_sum;
     }
 };
