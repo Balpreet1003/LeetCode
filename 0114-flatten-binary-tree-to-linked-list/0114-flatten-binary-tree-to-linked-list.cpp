f7c67d852e3540d79bacfc5320f44357420b10f5
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-19 22:47:18 +0530

    Time: 0 ms (100%), Space: 17.4 MB (77.13%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,21 +12,22 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        TreeNode*curr=root;
+        if(!root)
+            return ;
+        TreeNode* curr = root;
         while(curr){
-            if(curr->left){
-                TreeNode*pred=curr->left;
-                while(pred->right){
-                    pred=pred->right;
-                }
-                pred->right=curr->right;
-                curr->right=curr->left;
-                curr->left=NULL;
+            if(!curr->left)
+                curr = curr->right;
+            else {
+                TreeNode* prev = curr->left;
+                while(prev->right)
+                    prev = prev->right;
+                
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+                curr = curr->right;
             }
-            curr=curr->right;
         }
     }
 };
