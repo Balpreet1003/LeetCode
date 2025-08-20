50cf725e19acfc24cd6fcfddda49fb7c2f91732c
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-20 20:16:57 +0530

    Time: 23 ms (8.39%), Space: 31.5 MB (7.19%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -17,7 +17,7 @@ public:
         int n = matrix[0].size();
         int ans = 0;
         
-        dp.resize(n+1, vector<int>(m+1, -1));
+        dp.resize(m+1, vector<int>(n+1, -1));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 ans += helper(matrix, i, j);
