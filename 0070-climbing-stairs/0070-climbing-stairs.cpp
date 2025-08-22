2933b88ecc064d9929b217cc0bb5d810089d014b
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-08-22 16:19:42 +0530

    Time: 0 ms (100%), Space: 8 MB (59.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
+    vector<int>dp;
 public:
     int climbStairs(int n) {
-        //intial condition
-        int ptr1=1;
-        int ptr2=1;
-        int ans=ptr1;
-        for(int i=2;i<=n;i++){
-            ans=ptr1+ptr2;
-            ptr2=ptr1;
-            ptr1=ans;
+        int prev1=1, prev2=-1;
+        for(int i=1;i<=n;i++){
+            int curr=prev1;
+            if(i>1)
+                curr+=prev2;
+            prev2=prev1;
+            prev1=curr;
         }
-        return ans;
+        return prev1;
     }
 };
