b79e4a447dd24acb1263492a1b863dd3a4f3193d
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-04 20:44:40 +0530

    Time: 313 ms (6.64%), Space: 117.3 MB (5.31%) - LeetHub

---
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximumGood(vector<vector<int>>& statements) {
+        int n=statements.size(), ans=0;
+
+        for(int i=0;i<(1<<n);i++){
+            vector<int>arr(n,-1);
+            int cnt=0;
+            for(int j=0;j<16;j++){
+                if((i>>j)&1){
+                    arr[j]=1;
+                    cnt++;
+                }
+            }
+            bool flag=true;
+            for(int j=0;j<n;j++){
+                if(arr[j]==1){
+                    for(int k=0;k<n;k++){
+                        if(k==j)
+                            continue;
+                        else{
+                            if(statements[j][k]==1){
+                                if(arr[k]==-1){
+                                    flag=false;
+                                    break;
+                                }
+                            }
+                            else if(statements[j][k]==0){
+                                if(arr[k]==1){
+                                    flag=false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if(!flag)
+                        break;
+                }
+            }
+            if(flag)
+                ans=max(ans, cnt);
+        }
+        return ans;
+    }
+};
