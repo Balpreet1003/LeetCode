96edea94ce15133d958cb0437a6739ab88d7dc96
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-06 19:46:26 +0530

    Time: 3 ms (73.3%), Space: 19.5 MB (7.79%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,37 @@
 class Solution {
-public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj_list(n);
-        vector<int>indegree(n,0);
-        vector<int>topo_sort;
-        queue<int>q;
-
-        for(auto x:prerequisites){
-            adj_list[x[1]].push_back(x[0]);
-        }
-
-        for(int i=0;i<n;i++)
-            for(auto x:adj_list[i])indegree[x]++;
-
-        for(int i=0;i<n;i++)
-            if(indegree[i]==0)q.push(i);
-
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-
-            topo_sort.push_back(node);
-            for(auto x:adj_list[node]){
-                indegree[x]--;
-                if(indegree[x]==0)q.push(x);
+    stack<int>st;
+    bool isCycle(unordered_map<int, vector<int>>& adj_list, vector<bool>&vis, vector<bool>&pathVis, int i){
+        vis[i]=true;
+        pathVis[i]=true;
+        for(auto x:adj_list[i]){
+            if(!vis[x]){
+                if(isCycle(adj_list, vis, pathVis, x))
+                    return true;
             }
+            else if(pathVis[x])
+                return true;
         }
-        if(topo_sort.size()<n)topo_sort={};
-        return  topo_sort;
+        pathVis[i]=false;
+        st.push(i);
+        return false;
+    }
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>>adj_list;
+        for(auto x:prerequisites){
+            adj_list[x[0]].push_back(x[1]);
+        }
+        vector<bool>vis(numCourses,false), pathVis(numCourses,false);
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i] && isCycle(adj_list, vis, pathVis, i))
+                return {};
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
