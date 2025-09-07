db2c3004e783bb0d78d2e2619e518d5ff30f0c4b
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-07 18:07:13 +0530

    Time: 83 ms (14.26%), Space: 42.2 MB (12.27%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,17 +7,17 @@ public:
         if (grid[0][0] == 1) return -1;
         if (n == 1) return 1;
 
-        queue<pair<pair<int, int>, int>> q;
-        q.push({{0, 0}, 1});
+        queue<vector<int>> q;
+        q.push({0, 0, 1});
         grid[0][0] = 1;
 
         while (!q.empty()) {
             auto curr = q.front();
             q.pop();
 
-            int x = curr.first.first;
-            int y = curr.first.second;
-            int h = curr.second;
+            int x = curr[0];
+            int y = curr[1];
+            int h = curr[2];
 
             if (x == n-1 && y == n-1) return h;
 
@@ -25,7 +25,7 @@ public:
                 int i = x + dx[l];
                 int j = y + dy[l];
                 if (i >= 0 && i < n && j >= 0 && j < n && grid[i][j] == 0) {
-                    q.push({{i, j}, h + 1});
+                    q.push({i, j, h + 1});
                     grid[i][j] = 1;
                 }
             }
