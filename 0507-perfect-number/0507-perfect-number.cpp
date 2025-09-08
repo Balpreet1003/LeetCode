692f6ec3585dde1c15fbaef1b0b6079fa1f418db
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-08 19:29:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (23.94%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        set<int>arr;
+        if(num==1)
+            return false;
+        //set<int>arr;
+        int sum=-num;
         for(int i=1;i*i<=num;i++){
             if(num%i==0){
-                arr.insert(i);
-                arr.insert(num/i);
+                //arr.insert(i);
+                //arr.insert(num/i);
+                sum+=i;
+                sum+=(num/i);
             }
         }
-        int sum=-num;
-        for(int i:arr)sum+=i;
+        // for(int i:arr)sum+=i;
         return sum==num;
     }
 };
