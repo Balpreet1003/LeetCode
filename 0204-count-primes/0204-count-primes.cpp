c2b153fdf28ff804943ebe6881a1440a1095903a
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-08 18:49:58 +0530

    Time: 202 ms (44.16%), Space: 12.6 MB (66.35%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,16 +4,18 @@ public:
         if(n==0 || n==1)
             return 0;
         vector<bool>isPrime(n,true);
+        int ans=0;
         for(int i=2;i<n;i++){
             if(isPrime[i]){
+                ans++; 
                 for(long long j=i*1LL*i;j<n;j+=i){
                     isPrime[j]=false;
                 }
             }
         }
-        int ans=0;
-        for(int i=2;i<n;i++)
-            if(isPrime[i])ans++;
+        // int ans=0;
+        // for(int i=2;i<n;i++)
+        //     if(isPrime[i])ans++;
         return ans;
     }
 };
