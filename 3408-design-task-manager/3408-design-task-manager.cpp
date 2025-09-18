e5c73700b6b2d6a9816da9895d8f21c5bb55f113
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-18 18:51:17 +0530

    Time: 188 ms (94.69%), Space: 342 MB (99.69%) - LeetHub

--- 3408-design-task-manager/3408-design-task-manager.cpp
+++ 3408-design-task-manager/3408-design-task-manager.cpp
@@ -1,52 +1,43 @@
 class TaskManager {
-    struct Task {
-        int priority, taskId, userId;
-    };
-
-    struct cmp {
-        bool operator()(const Task& a, const Task& b) const {
-            if (a.priority == b.priority)
-                return a.taskId > b.taskId;
-            return a.priority < b.priority;
-        }
-    };
-
-    priority_queue<Task, vector<Task>, cmp> tasks_list;
-    unordered_map<int, int> trace;
-
+    using int2=pair<int , int>; 
+    unordered_map<int, int2> mp;// taskID->(prioity, userId)
+    priority_queue<int2> pq; //(priority, taskID)
 public:
     TaskManager(vector<vector<int>>& tasks) {
-        for (auto& x : tasks) {
-            tasks_list.push({x[2], x[1], x[0]});
-            trace[x[1]] = x[2];
+        mp.reserve(tasks.size());
+        for(auto& t: tasks){
+            int  u=t[0], i=t[1], p=t[2];
+            mp[i]={p, u};
+            pq.emplace(p, i);
         }
     }
     
     void add(int userId, int taskId, int priority) {
-        tasks_list.push({priority, taskId, userId});
-        trace[taskId] = priority;
+        mp[taskId]={priority, userId};
+        pq.emplace(priority, taskId);
     }
     
     void edit(int taskId, int newPriority) {
-        trace[taskId] = newPriority;
+        mp[taskId].first=newPriority;
+        pq.emplace(newPriority, taskId);
     }
     
     void rmv(int taskId) {
-        trace.erase(taskId);
+        mp[taskId].first=-1;
     }
     
     int execTop() {
-        while (!tasks_list.empty() &&
-              (!trace.count(tasks_list.top().taskId) ||
-               trace[tasks_list.top().taskId] != tasks_list.top().priority)) {
-            auto x = tasks_list.top();
-            tasks_list.pop();
-            if (trace.count(x.taskId))
-                tasks_list.push({trace[x.taskId], x.taskId, x.userId});
+        while (!pq.empty()) {
+            auto [p, i] = pq.top();
+            if (mp.count(i) && mp[i].first == p) {
+                pq.pop();
+                int u=mp[i].second;
+                mp.erase(i); // Remove task after execution
+                return u;
+            }
+            pq.pop(); // Remove
         }
-
-        int ans = tasks_list.top().userId;
-        tasks_list.pop();
-        return ans;
+        return -1; // No valid tasks to execute
+        
     }
 };
