36aedcc9bace4b4f9e9a045bad6bfd10c04990fb
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-09-26 23:51:36 +0530

    Time: 267 ms (58.41%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    CASE 
+        WHEN COUNT(*) = 0 THEN NULL
+        ELSE salary
+    END AS SecondHighestSalary
+FROM (
+    SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM Employee
+) t
+WHERE rnk = 2;
