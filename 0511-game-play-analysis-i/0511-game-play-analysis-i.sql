a30a43caabe43395521ceb382fb78bf858bbb16e
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-10-03 12:56:43 +0530

    Time: 583 ms (18.58%), Space: 0B (100%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+select 
+    player_id,
+    first_login
+from 
+    (
+        select 
+            player_id,
+            event_date as first_login,
+            row_number() over(partition by player_id order by event_date) as rn
+        from
+            Activity
+    )t
+where 
+    rn=1;
