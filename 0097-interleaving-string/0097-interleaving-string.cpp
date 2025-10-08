8977920ccc6b5bac927cc6a4073425f0a7a775c8
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-10-08 19:44:59 +0530

    Time: 172 ms (5.16%), Space: 88 MB (5.74%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,19 +1,19 @@
 class Solution {
     int n1, n2, m;
     vector<vector<vector<int>>> dp;
-    bool solve(string& s1, string& s2, string& s3, int k, int i, int j) {
-        if (k == m)
-            return true;
-        if (dp[k][i][j] != -1)
-            return dp[k][i][j];
+    // bool solve(string& s1, string& s2, string& s3, int k, int i, int j) {
+    //     if (k == m)
+    //         return true;
+    //     if (dp[k][i][j] != -1)
+    //         return dp[k][i][j];
 
-        bool ans = false;
-        if (i < n1 && s3[k] == s1[i])
-            ans |= solve(s1, s2, s3, k + 1, i + 1, j);
-        if (j < n2 && s3[k] == s2[j])
-            ans |= solve(s1, s2, s3, k + 1, i, j + 1);
-        return dp[k][i][j] = ans;
-    }
+    //     bool ans = false;
+    //     if (i < n1 && s3[k] == s1[i])
+    //         ans |= solve(s1, s2, s3, k + 1, i + 1, j);
+    //     if (j < n2 && s3[k] == s2[j])
+    //         ans |= solve(s1, s2, s3, k + 1, i, j + 1);
+    //     return dp[k][i][j] = ans;
+    // }
 
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -22,40 +22,21 @@ public:
         m = s3.length();
         if (n1 + n2 != m)
             return false;
-        dp.resize(m + 1, vector<vector<int>>(n1 + 1, vector<int>(n2 + 1, -1)));
-        return solve(s1, s2, s3, 0, 0, 0);
+        dp.resize(m + 1, vector<vector<int>>(n1 + 1, vector<int>(n2 + 1, 0)));
+        // vector<vector<int>>prev(n1+1, vector<int>(n2+1, false)), curr(n1+1, vector<int>(n2+1, false))
+        dp[m][n1][n2]=true;
+        for(int k=m-1;k>=0;k--){
+            for(int i=n1;i>=0;i--){
+                for(int j=n2;j>=0;j--){
+                    bool ans = false;
+                    if (i < n1 && s3[k] == s1[i])
+                        ans |= dp[k + 1][i + 1][j];
+                    if (j < n2 && s3[k] == s2[j])
+                        ans |= dp[k + 1][i][j + 1];
+                    dp[k][i][j] = ans;
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
-
-// class Solution {
-//     int n1, n2, m;
-//     vector<vector<vector<int>>> dp;
-
-//     bool solve(string& s1, string& s2, string& s3, int k, int i, int j) {
-//         if (k == m)
-//             return true;
-//         if (dp[k][i][j] != -1)
-//             return dp[k][i][j];
-
-//         bool ok = false;
-//         if (i < n1 && s3[k] == s1[i])
-//             ok = ok || solve(s1, s2, s3, k + 1, i + 1, j);
-//         if (j < n2 && s3[k] == s2[j])
-//             ok = ok || solve(s1, s2, s3, k + 1, i, j + 1);
-
-//         return dp[k][i][j] = ok;
-//     }
-
-// public:
-//     bool isInterleave(string s1, string s2, string s3) {
-//         n1 = s1.length();
-//         n2 = s2.length();
-//         m = s3.length();
-
-//         if (n1 + n2 != m)
-//             return false; // basic length mismatch check
-
-//         dp.assign(m + 1, vector<vector<int>>(n1 + 1, vector<int>(n2 + 1, -1)));
-//         return solve(s1, s2, s3, 0, 0, 0);
-//     }
-// };
