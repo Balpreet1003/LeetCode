04ac0d7f6ded3597482a32c3072cf54e630a3dc9
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-10-08 19:46:49 +0530

    Time: 130 ms (5.16%), Space: 10.5 MB (27.74%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,42 +1,26 @@
 class Solution {
-    int n1, n2, m;
-    vector<vector<vector<int>>> dp;
-    // bool solve(string& s1, string& s2, string& s3, int k, int i, int j) {
-    //     if (k == m)
-    //         return true;
-    //     if (dp[k][i][j] != -1)
-    //         return dp[k][i][j];
-
-    //     bool ans = false;
-    //     if (i < n1 && s3[k] == s1[i])
-    //         ans |= solve(s1, s2, s3, k + 1, i + 1, j);
-    //     if (j < n2 && s3[k] == s2[j])
-    //         ans |= solve(s1, s2, s3, k + 1, i, j + 1);
-    //     return dp[k][i][j] = ans;
-    // }
-
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        n1 = s1.length();
-        n2 = s2.length();
-        m = s3.length();
+        int n1 = s1.length(), n2 = s2.length(), m = s3.length();
         if (n1 + n2 != m)
             return false;
-        dp.resize(m + 1, vector<vector<int>>(n1 + 1, vector<int>(n2 + 1, 0)));
-        // vector<vector<int>>prev(n1+1, vector<int>(n2+1, false)), curr(n1+1, vector<int>(n2+1, false))
-        dp[m][n1][n2]=true;
+        if(n1==0 && n2==0 && m==0)
+            return true;
+        vector<vector<int>>prev(n1+1, vector<int>(n2+1, 0)), curr(n1+1, vector<int>(n2+1, 0));
+        prev[n1][n2]=true;
         for(int k=m-1;k>=0;k--){
-            for(int i=n1;i>=0;i--){
-                for(int j=n2;j>=0;j--){
+            for(int i=0;i<=n1;i++){
+                for(int j=0;j<=n2;j++){
                     bool ans = false;
                     if (i < n1 && s3[k] == s1[i])
-                        ans |= dp[k + 1][i + 1][j];
+                        ans |= prev[i + 1][j];
                     if (j < n2 && s3[k] == s2[j])
-                        ans |= dp[k + 1][i][j + 1];
-                    dp[k][i][j] = ans;
+                        ans |= prev[i][j + 1];
+                    curr[i][j] = ans;
                 }
             }
+            prev=curr;
         }
-        return dp[0][0][0];
+        return curr[0][0];
     }
 };
