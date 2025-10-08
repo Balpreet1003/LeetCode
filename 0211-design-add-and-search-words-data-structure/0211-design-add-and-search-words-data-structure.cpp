bc7c403e583c29db8292414be69b995e1c6b9a2d
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-10-08 18:09:37 +0530

    Time: 447 ms (58.15%), Space: 628.3 MB (18.46%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    vector<Node*> children;
+    bool is_leaf_node;
+    
+    Node() {
+        children = vector<Node*>(26, nullptr);
+        is_leaf_node = false;
+    }
+};
+
+class WordDictionary {
+    Node* root;
+public:
+    WordDictionary() : root(new Node()) {}
+
+    void addWord(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word[i];
+            if (!node->children[ch - 'a'])
+                node->children[ch - 'a'] = new Node();
+            node = node->children[ch - 'a'];
+        }
+        // ✅ mark the last node as leaf
+        node->is_leaf_node = true;
+    }
+
+    bool search_helper(string& s, int i, Node* node) {
+        if (!node) return false;
+        if (i >= s.length()) return node->is_leaf_node;
+
+        if (s[i] == '.') {
+            for (int j = 0; j < 26; j++) {   // ✅ avoid shadowing `i`
+                if (node->children[j] && search_helper(s, i + 1, node->children[j]))
+                    return true;
+            }
+            return false;
+        }
+
+        if (!node->children[s[i] - 'a'])
+            return false;
+
+        return search_helper(s, i + 1, node->children[s[i] - 'a']);
+    }
+
+    bool search(string word) {
+        return search_helper(word, 0, root);
+    }
+};
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
