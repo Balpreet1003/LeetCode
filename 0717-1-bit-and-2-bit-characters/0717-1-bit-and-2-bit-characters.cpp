e4a5d48c4fb2e2749a52b8ab545406eeaa96529c
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-11-18 20:36:43 +0530

    Time: 0 ms (100%), Space: 13.1 MB (41.51%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        for(int i=0;i<bits.size();){
+            if(i==bits.size()-1)
+                return true;
+            if(bits[i]==0){
+                i++;
+            }
+            else{
+                i+=2;
+            }
+        }
+        return false;
+    }
+};
