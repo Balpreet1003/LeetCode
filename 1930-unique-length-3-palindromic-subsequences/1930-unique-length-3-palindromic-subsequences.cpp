852a7095d192a849e80640d04ecc8b1b29f86dc9
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2025-11-22 14:27:18 +0530

    Time: 51 ms (91.59%), Space: 14.2 MB (92.5%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,33 +1,39 @@
 class Solution {
+    int first_index(string& s, char& c){
+        for(int i=0;i<s.length();i++){
+            if(s[i]==c)
+                return i;
+        }
+        return -1;
+    }
+    int last_index(string& s, char& c){
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]==c)
+                return i;
+        }
+        return -1;
+    }
+    int get_unique(string& s, int l, int r){
+        vector<int>cnt(26, 0);
+        for(int i=l+1; i<r; i++){
+            cnt[s[i]-'a']++;
+        }
+        int ans=0;
+        for(int i:cnt){
+            if(i>0)ans++;
+        }
+        return ans;
+    }
 public:
     int countPalindromicSubsequence(string s) {
-        int n = s.length();
-        vector<int> first(26, -1), last(26, -1);
-        unordered_set<string> uniquePalindromes;
-
-        // Record first and last occurrence of each character
-        for (int i = 0; i < n; ++i) {
-            if (first[s[i] - 'a'] == -1) {
-                first[s[i] - 'a'] = i;
-            }
-            last[s[i] - 'a'] = i;
-        }
-
-        int count = 0;
-
-        // For each character, calculate unique middle characters
-        for (char c = 'a'; c <= 'z'; ++c) {
-            int f = first[c - 'a'];
-            int l = last[c - 'a'];
-            if (f != -1 && f + 1 < l) {
-                unordered_set<char> middleChars;
-                for (int i = f + 1; i < l; ++i) {
-                    middleChars.insert(s[i]);
-                }
-                count += middleChars.size();
+        int ans=0;
+        for(char c='a';c<='z';c++){
+            int left=first_index(s, c);
+            int right=last_index(s, c);
+            if(left!=-1 && right!=-1 && right-left>1){
+                ans += get_unique(s, left, right);
             }
         }
-
-        return count;
+        return ans;
     }
 };
