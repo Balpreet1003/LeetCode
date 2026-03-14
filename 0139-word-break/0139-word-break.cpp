ba15e0d4dd38e5f6a5220a856ab1a649f595cb2c
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-03-15 01:11:25 +0530

    Time: 3 ms (71.11%), Space: 12 MB (69.74%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,29 +1,28 @@
-class Solution { 
+class Solution {
+    unordered_map<string, int>mp;
     vector<int>dp;
-    bool solve(string&s,unordered_map<string,bool>&m,int i){
-        if(i>=s.length()){
+    bool solve(string& s, int i){
+        if(i>=s.length())
             return true;
-        }
-
-        if(dp[i]!=-1)return dp[i];
-
-        string s1="";
+        if(dp[i]!=-1)
+            return dp[i];
         bool ans=false;
+        string s1="";
         for(int j=i;j<s.length();j++){
             s1+=s[j];
-            if(m.find(s1)!=m.end()){
-                ans = ans || solve(s,m,j+1);
-            }
+            if(mp.count(s1)){
+                ans|=solve(s, j+1);
+                if(ans)
+                    return dp[i]=ans;
+            }  
         }
-        return dp[i] = ans;
+        return dp[i]=false;
     }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,bool>m;
-        for(auto i:wordDict)m[i]=false;
-        
-        dp=vector<int>(s.length()+1,-1);
-
-        return solve(s,m,0);
+        for(auto x:wordDict)
+            mp[x]=1;
+        dp.resize(s.length()+1, -1);
+        return solve(s, 0);
     }
 };
