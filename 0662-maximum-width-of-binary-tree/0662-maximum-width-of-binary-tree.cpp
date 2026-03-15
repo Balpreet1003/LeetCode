2c620e9ad1fa148b17f08198d5060a4acc8b329e
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-03-15 13:31:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,31 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    unordered_map<int, pair<int, int>>mp;
+    void solve(TreeNode* root){
+        queue<pair<TreeNode*, pair<int, int>>>q;
+        q.push({root, {0, 0}});
+        while(!q.empty()){
+            auto& [node, x]=q.front();
+            auto& [l, h]=x;
+            q.pop();
+            if(mp.count(h)){
+                mp[h].first=min(mp[h].first, l);
+                mp[h].second=max(mp[h].second, l);
+            }
+            else{
+                mp[h]={l, l};
+            }
+
+            if(node->left)
+                q.push({node->left, {2*l, h+1}});
+            if(node->right)
+                q.push({node->right, {2*l+1, h+1}});
+        }
+    }
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (!root) return 0;
-        
-        long long ans = 0;
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
-        
-        while (!q.empty()) {
-            int size = q.size();
-            long long left = q.front().second;
-            long long right = left;
-            
-            for (int i = 0; i < size; i++) {
-                auto [node, idx] = q.front(); q.pop();
-                idx -= left;
-                right = idx;
-                
-                if (node->left) 
-                    q.push({node->left, 2*idx});
-                if (node->right) 
-                    q.push({node->right, 2*idx+1});
-            }
-            
-            ans = max(ans, right + 1);
+        solve(root);
+        int ans=0;
+        for(auto& [x, y]:mp){
+            cout<<y.second<<" - "<<y.first<<endl;
+            ans=max(ans, y.second-y.first+1);
         }
-        
-        return (int)ans;
+        return ans;
     }
 };
