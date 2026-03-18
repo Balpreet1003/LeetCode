cb28645d9c431f57a88a34a6a278f5e2085372ff
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-03-19 00:03:57 +0530

    Time: 77 ms (11.24%), Space: 167.9 MB (12.11%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& t) {
+        bool ans[3]={false, false, false};
+        for(auto x:triplets){
+            if(x[0]<=t[0] && x[1]<=t[1] && x[2]<=t[2]){
+                if(x[0]==t[0])
+                    ans[0]=true;
+                if(x[1]==t[1])
+                    ans[1]=true;
+                if(x[2]==t[2])
+                    ans[2]=true;
+            }
+        }
+        return ans[0] && ans[1] && ans[2];
+    }
+};
