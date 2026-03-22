e4ca00ec6fb94b73e7374f5ec5cce60366bfd1ab
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-03-22 23:43:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int change(int am, vector<int>& c){
-        int n=c.size();
-        vector<long long>prev(am+1,0),curr(am+1,0);
-        prev[0]=1;
-        curr[0]=1;
-        for(int i=0;i<n;i++){
-            for(int a=1;a<=am;a++){
-                int exclude=0;
-                if(i>0)exclude=prev[a];
-                int include=0;
-                if(c[i]<=a)include=curr[a-c[i]];
-                curr[a]=(long long)include+exclude;
+    int change(int amount, vector<int>& coins) {
+        vector<unsigned long long> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for(int coin : coins) {
+            for(int x = coin; x <= amount; x++) {
+                dp[x] += dp[x - coin];
             }
-            prev=curr;
         }
-        return curr[am];
+        return dp[amount];
     }
 };
