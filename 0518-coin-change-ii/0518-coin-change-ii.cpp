bf03e34551af8e8c85223359755278953380a6a1
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-03-22 23:49:00 +0530

    Time: 47 ms (23.37%), Space: 61.7 MB (9.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,14 +1,19 @@
 class Solution {
+    vector<vector<unsigned long long>>dp;
 public:
-    int change(int amount, vector<int>& coins) {
-        vector<unsigned long long> dp(amount + 1, 0);
-        dp[0] = 1;
-
-        for(int coin : coins) {
-            for(int x = coin; x <= amount; x++) {
-                dp[x] += dp[x - coin];
+    int change(int amount, vector<int>& nums) {
+        int n=nums.size();
+        dp.assign(n+1, vector<unsigned long long>(amount+1, 0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int x=1;x<=amount;x++){
+                if(nums[i]<=x)
+                    dp[i][x]+=dp[i][x-nums[i]];
+                dp[i][x]+=dp[i+1][x];
             }
         }
-        return dp[amount];
+        return dp[0][amount];
     }
 };
