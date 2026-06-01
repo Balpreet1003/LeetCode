0b9a571cf28ac0b638b81df42753af1cb82c729c
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-02 02:31:20 +0530

    Time: 1105 ms (91.87%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select b.product_name, a.year, a.price
-from Sales as a left join Product as b on 
-a.product_id = b.product_id;
+from
+    Sales as a 
+    left join 
+    Product as b
+    on a.product_id = b.product_id;
