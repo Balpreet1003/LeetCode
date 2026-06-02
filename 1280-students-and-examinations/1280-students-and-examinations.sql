0f41811e56a0b7daae386012e193e101054b5361
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-03 00:57:57 +0530

    Time: 411 ms (31.18%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,7 +1,29 @@
-# Write your MySQL query statement below
-select a.student_id, a.student_name, c.subject_name, 
-    count(b.subject_name) as attended_exams
-from Students as a cross join Subjects as c left join Examinations as b
-on a.student_id=b.student_id and b.subject_name=c.subject_name
-group by a.student_id, a.student_name, c.subject_name
-order by a.student_id, c.subject_name;
+-- Write your PostgreSQL query statement below
+with student_table2 as (
+    select 
+        a.student_id,
+        b.subject_name,
+        a.student_name
+    from
+        Students as a
+        cross join
+        Subjects as b
+)
+
+select
+    a.student_id,
+    a.student_name,
+    a.subject_name,
+    count(b.student_id) as attended_exams
+from
+    student_table2 as a
+    left join
+    Examinations as b
+    on a.student_id = b.student_id and a.subject_name = b.subject_name
+group by
+    a.student_id,
+    a.subject_name,
+    a.student_name
+order by
+    a.student_id,
+    a.student_name;
