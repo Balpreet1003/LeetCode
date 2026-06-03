e5c902418e195c39eb52f15163e1e33c1939a754
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-04 00:09:52 +0530

    Time: 261 ms (28.45%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,18 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select 
-    a.user_id, 
-    ifnull(round(count(case when b.action="confirmed" then 1 end)/count(b.action), 2),0) as confirmation_rate
-from 
-    Signups as a left join Confirmations as b on a.user_id=b.user_id
-group by 
-    a.user_id;
+    a.user_id,
+    round(
+        avg(
+            CASE
+                WHEN b.action = 'confirmed' THEN 1.0
+                ELSE 0.0
+            END
+        ), 2
+    ) as confirmation_rate
+from
+    Signups as a
+    left join
+    Confirmations as b
+    on a.user_id = b.user_id
+group by
+    a.user_id
