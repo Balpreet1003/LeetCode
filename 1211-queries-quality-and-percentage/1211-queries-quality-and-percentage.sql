a13d85b50a56d3d3b9f249c1c23946851d5990f0
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-05 00:04:47 +0530

    Time: 339 ms (5.03%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,6 +1,9 @@
-# Write your MySQL query statement below
-SELECT query_name, 
-ROUND(AVG(CAST(rating AS DECIMAL) / position),2) AS quality, 
-ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100 / COUNT(*), 2) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name;
+-- Write your PostgreSQL query statement below
+select
+    query_name,
+    round(avg(rating::numeric/position::numeric), 2) as quality,
+    round(100*count(*)filter(where rating < 3)::numeric/count(*), 2) as poor_query_percentage
+from
+    Queries
+group by
+    query_name
