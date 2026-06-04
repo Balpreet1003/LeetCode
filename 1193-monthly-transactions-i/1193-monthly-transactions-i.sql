7c523a3529bb7b05c3c4d54992aa70efcd915fcc
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-05 00:15:35 +0530

    Time: 420 ms (57.05%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,5 +1,13 @@
-# Write your MySQL query statement below
-select date_format(trans_date, '%Y-%m') as month, country, count(id) as trans_count, sum(if(state='approved', 1, 0)) as approved_count,
-sum(amount) as trans_total_amount, sum(if(state='approved', amount, 0)) as approved_total_amount
-from Transactions
-group by country, date_format(trans_date, '%Y-%m');
+-- Write your PostgreSQL query statement below
+select
+    to_char(trans_date, 'YYYY-MM') as month,
+    country,
+    count(*) as trans_count,
+    count(*) filter(where state='approved') as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state='approved' then amount else 0 end) as approved_total_amount
+from
+    Transactions
+group by
+    country,
+    month
