21d205db4530c4e440d9cb18acc08f6048102890
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-06 15:20:57 +0530

    Time: 365 ms (88.46%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,8 +1,15 @@
-# Write your MySQL query statement below
-Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
-from Delivery
-where (customer_id, order_date) in (
-  Select customer_id, min(order_date) 
-  from Delivery
-  group by customer_id
-);
+-- Write your PostgreSQL query statement below
+select
+    round(
+        count(*) filter(where order_date = customer_pref_delivery_date)::numeric*100/
+        count(*)
+    , 2)as immediate_percentage
+from
+    Delivery where (customer_id, order_date) in (
+        select
+            customer_id,
+            min(order_date)
+        from Delivery
+        group by customer_id
+    )
+     
