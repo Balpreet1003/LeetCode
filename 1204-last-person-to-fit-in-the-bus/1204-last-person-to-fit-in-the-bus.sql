8a5e82bd37a2947309a2fdcd0cb9ca8e0e769016
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-07 05:16:30 +0530

    Time: 434 ms (66.44%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,12 @@
 -- Write your PostgreSQL query statement below
-select a.person_name
-from Queue as a inner join Queue as b
-on a.turn >= b.turn
-group by a.turn, a.person_name
-having sum(b.weight)<=1000
-order by sum(b.weight) desc
-limit 1;
+select person_name
+from (
+    select
+        person_name,
+        turn,
+        sum(weight) over(order by turn) as sum
+    from Queue
+)
+where sum<=1000
+order by turn desc
+limit 1
