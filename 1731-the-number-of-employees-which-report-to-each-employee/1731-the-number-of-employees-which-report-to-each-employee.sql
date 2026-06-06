09d6d7b5f97f22e39ab9cb1da3405bf8694b7691
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-06 16:15:17 +0530

    Time: 326 ms (57.39%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,5 +1,16 @@
-select a.employee_id,a.name,count(*) as reports_count,round(avg(b.age*1.0),0) as average_age
-from Employees a inner join Employees b
-on a.employee_id=b.reports_to
-group by a.employee_id,a.name
-order by a.employee_id
+-- Write your PostgreSQL query statement below
+select
+    a.employee_id, 
+    a.name,
+    count(b.reports_to) as reports_count,
+    round(avg(b.age)) as average_age
+from
+    Employees as a
+    join 
+    Employees as b
+    on a.employee_id = b.reports_to and b.reports_to is not null
+group by 
+    a.employee_id, 
+    a.name
+order by
+    a.employee_id
