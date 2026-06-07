86907fb71da47c7348cbbeb709b4979ba9a17587
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-07 05:52:50 +0530

    Time: 175 ms (97.6%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,9 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 select 
-case 
-when id%2=0 then id-1
-when id%2=1 and id=(select max(id) from Seat) then id
-else id+1
-end as id,student
+    case 
+        when id=(select max(id) from Seat) and mod(id, 2)=1
+            then id
+        when mod(id, 2)=1
+            then id+1
+        else 
+            id-1
+    end as id, student
 from Seat
-order by id;
+order by id
