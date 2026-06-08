99d18a49adda249362586f5137aebbc4e5742aa5
Balpreet Singh Gill <158618788+Balpreet1003@users.noreply.github.com>
2026-06-08 18:15:46 +0530

    Time: 201 ms (17.69%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,10 +1,24 @@
-# Write your MySQL query statement below
-SELECT id, COUNT(*) AS num
-FROM (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id AS id FROM RequestAccepted
-) AS all_ids
-GROUP BY id
-ORDER BY num DESC
-LIMIT 1;
+-- Write your PostgreSQL query statement below
+with table1 as (
+    select 
+        requester_id as id,
+        count(*) as cnt
+    from RequestAccepted
+    group by requester_id
+
+    union all
+
+    select 
+        accepter_id as id,
+        count(*) as cnt
+    from RequestAccepted
+    group by accepter_id
+)
+
+select 
+    id,
+    sum(cnt) as num
+from table1
+group by id
+order by num desc
+limit 1
